d45352c71b304f7e5e6ee029715c7cf50da5e21c
Mayank Shekhar <mayankshekhar03@gmail.com>
2017-07-22 14:46:27 +0530

    member func showSeq tested

--- magic/index.js
+++ magic/index.js
@@ -1,35 +1,47 @@
 //stores the current sequence of colors press
-var seq    = [];
+var seq    = [1,2,3,2,1];
 var sounds = ['https://s3.amazonaws.com/freecodecamp/simonSound1.mp3',
               'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
               'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3',
               'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'];
+var count = [];
 var counter = 0;
+var round = 0;
 
 $(document).ready(function(){
-    $('#start').click(startGame);
+    $('#start').click(showSeq);
 });
 // gets the game started with one sequence and calls a function which handles inputs
 function startGame() {
+    round++;
     var color = (Math.floor(Math.random() * 4)) + 1;
     seq = [];
+    count = [];
+    counter = 0;
     seq.push(color);
     console.log(seq);
-    for(var i=0;i<seq.length; i++) {
+    showSeq();
+}
+
+function showSeq() {
+    for(var i=0; i<seq.length; i++) {
         var id = '#' + seq[i];
-        $(id).css('opacity', '1');
-        new Audio(sounds[seq[i]]).play();
-        setTimeout(function() {opacity(id);}, 700);
+        toOpac(id, i, seq[i]);
+        toTrans(id, i+1);
     }
-    handleInput();
 }
 
-function handleInput() {
-    
+//helper functions
+function toOpac(id, i, s){
+    var timeout = i*700;
+    setTimeout(function() {$(id).css('opacity', '1');
+                           new Audio(sounds[s]).play();}, timeout);
 }
 
-//sets opacity to 0.5
+function toTrans(id, i){
+    var timeout = i*650;
+    setTimeout(function() {opacity(id);}, timeout);
+}
 function opacity(id) {
-    console.log(id);
-    $(id).css('opacity', '0.5');
+    $(id).css('opacity', '0.3');
 }
